5c421d031ebd8c81ed3d00f08415f02c8ce6a3ed
niceonionslice <niceonionslice@gmail.com>
2019-08-31 23:33:51 +0900

    add comments. add two testcode

--- SimpleTest.rb
+++ SimpleTest.rb
@@ -2,6 +2,11 @@ require "minitest/autorun"
 
 class SampleTest < Minitest::Test
   def test_sample
+    # a と b が等しければパス
     assert_equal 'RUBY', 'ruby'.upcase
+    # aが真であればパス
+    assert true
+    # bが偽であればパス
+    refute false
   end
 end
