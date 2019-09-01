890096852c8073fd457540eb197aef62d416530a
niceonionslice <niceonionslice@gmail.com>
2019-09-01 14:40:49 +0900

    add testcode

--- FizzBuzzTest.rb
+++ FizzBuzzTest.rb
@@ -9,5 +9,10 @@ class FizzBuzzTest < Minitest::Test
     assert_equal '1', fizz_buzz(1)
     assert_equal '2', fizz_buzz(2)
     assert_equal 'fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'buzz', fizz_buzz(5)
+    assert_equal 'fizz', fizz_buzz(6)
+    assert_equal '7', fizz_buzz(7)
+    assert_equal 'fizzbuzz', fizz_buzz(15)
   end
 end
